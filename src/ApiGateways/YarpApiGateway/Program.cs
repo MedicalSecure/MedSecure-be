4550baf015fff2956052994f0fd2ae25a49ef8da
jamelChadha <chadhajamel47@gmail.com>
2024-04-20 19:27:59 +0200

    Fix Yarp Apigateway code

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
+// Ajouter la configuration à partir de appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json", optional: false);
+
+// Ajouter la configuration à partir de appsettings.local.json pour Docker Compose
+builder.Configuration.AddJsonFile("appsettings.local.json", optional: true);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 
 builder.Services.AddRateLimiter(rateLimiterOptions =>
 {
