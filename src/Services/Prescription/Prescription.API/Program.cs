5fad358c59cf068bad4a6cef3958a976a3092738
hammadi azaiez <azaiezhammadii@gmail.com>
2024-05-30 14:01:55 +0100

    Fix/Fix ApiGateway, works in `http only` (#11)
    * fix: working api gateway but `HTTP Only`
    
    * fix: fix API GATEWAY settings import based on ENV
    
    * fix: restore Microservices + Fix cors in API Gateway
    
    * fix: add front end url

--- src/Services/Prescription/Prescription.API/Program.cs
+++ src/Services/Prescription/Prescription.API/Program.cs
@@ -1,60 +1,50 @@
-using Microsoft.OpenApi.Models;
-using Prescription.Application;
-using Prescription.Infrastructure;
+using Prescription.API;
 using Prescription.Infrastructure.Database.Extensions;
 
-namespace Prescription.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddInfrastructureServices(builder.Configuration);// Add infrastructure services layer
+builder.Services.AddApplicationServices(builder.Configuration);  // Add application services layer
+builder.Services.AddApiServices(builder.Configuration);  // Add API services layer
+
+//allow cross origin TODO REMOVE from here and place it in my API GATEWAY!!
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
 {
-    public class Program
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                      });
+});
+//build app
+var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+// Configure the HTTP request pipeline
+app.UseApiServices();  // Configure API-related middleware
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Prescription API V1");  // Configure Swagger UI endpoint
+    });
 
-            builder.Services.AddControllers();
-            builder.Services.AddInfrastructureServices(builder.Configuration);// Add infrastructure services layer
-            builder.Services.AddApplicationServices(builder.Configuration);  // Add application services layer
-            builder.Services.AddApiServices(builder.Configuration);  // Add API services layer
-
-            //allow cross origin TODO REMOVE from here and place it in my API GATEWAY!!
-            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(name: MyAllowSpecificOrigins,
-                                  policy =>
-                                  {
-                                      policy.WithOrigins("http://localhost:4200")
-                                       .AllowAnyHeader()
-                                       .AllowAnyMethod();
-                                  });
-            });
-            //build app
-            var app = builder.Build();
-
-            app.UseCors(MyAllowSpecificOrigins);
-            // Configure the HTTP request pipeline
-            app.UseApiServices();  // Configure API-related middleware
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Prescription API V1");  // Configure Swagger UI endpoint
-                });
-
-                // Initialize the database asynchronously with mock data
-                app.InitialiseDatabaseAsync(true);
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    // Initialize the database asynchronously with mock data
+    await app.InitialiseDatabaseAsync(true);
 }
+
+app.MapControllers();
+
+app.Run();
