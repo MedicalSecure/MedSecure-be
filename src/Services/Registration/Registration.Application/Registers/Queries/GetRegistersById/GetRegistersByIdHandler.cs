0222d2c1e3e259113e865931a996dbc13c9942e0
hammadi azaiez <azaiezhammadii@gmail.com>
2024-06-01 14:47:57 +0100

    feat: add getRegisterById

---
+++ src/Services/Registration/Registration.Application/Registers/Queries/GetRegistersById/GetRegistersByIdHandler.cs
@@ -0,0 +1,43 @@
+﻿using Registration.Domain.Models;
+
+namespace Registration.Application.Registers.Queries.GetRegistersById
+{
+    public class GetRegistersByIdHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetRegistersByIdQuery, GetRegistersByIdResult>
+    {
+        public async Task<GetRegistersByIdResult> Handle(GetRegistersByIdQuery query, CancellationToken cancellationToken)
+
+        {
+            // get patients with pagination
+            // return result
+            var regId = query.Id;
+
+            if (regId == Guid.Empty)
+                throw new ArgumentNullException($"Register Id ${regId} is invalid");
+            try
+            {
+                var register = await dbContext.Registers
+                .Include(t => t.Patient)
+                .Include(t => t.Tests)
+                .Include(r => r.FamilyMedicalHistory)
+                .Include(r => r.PersonalMedicalHistory)
+                .Include(r => r.Disease)
+                .Include(r => r.Allergy)
+                .Include(t => t.History)
+                .FirstOrDefaultAsync(r => r.Id.Value == regId, cancellationToken);
+
+
+                if (register == null)
+                    throw new ArgumentNullException(nameof(register) + $" with id ${regId} is not found");
+                var registersDto = register.Status == RegisterStatus.Archived ? register.ToRegisterDto(isArchived: true) : register.ToRegisterDto(isArchived: false);
+
+                return new GetRegistersByIdResult(registersDto);
+            }
+            catch (Exception x)
+            {
+                throw new ArgumentNullException($"Register with id ${regId} is not found",x);
+            }
+            
+        }
+    }
+}
