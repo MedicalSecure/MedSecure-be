d396ccca907a9705d3b82f12f28bcfbc41f956a8
hammadi azaiez <azaiezhammadii@gmail.com>
2024-06-04 10:49:38 +0100

    feat: add registration event handler for Resident

---
+++ src/Services/Registration/Registration.Application/Histories/EventHandlers/Integration/ValidPrescriptionCreatedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.Messaging.Events.PrescriptionEvents;
+using Registration.Application.Histories.Commands.CreateHistory;
+
+namespace Registration.Application.Histories.EventHandlers.Integration
+{
+    public class ValidPrescriptionCreatedEventHandler(ISender sender) : IConsumer<ValidatedPrescriptionSharedEvent>
+    {
+        public async Task Consume(ConsumeContext<ValidatedPrescriptionSharedEvent> context)
+        {
+            var pres = context.Message;
+            var newResidentHistory = new HistoryDto(HistoryStatus.Resident, pres.RegisterId, null, DateTime.Now);//Id null to be created automatically
+            var command = new CreateHistoryCommand(newResidentHistory);
+            var response = await sender.Send(command);
+            Console.WriteLine($"New history created : {response.Id}");
+        }
+    }
+}
