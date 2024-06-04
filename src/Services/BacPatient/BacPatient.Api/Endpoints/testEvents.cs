406a1d681fe1a84664a3f5c1a995f2e48d26e8f2
hammadi azaiez <azaiezhammadii@gmail.com>
2024-06-04 14:51:14 +0100

    feat: add backpatient test setup

---
+++ src/Services/BacPatient/BacPatient.Api/Endpoints/testEvents.cs
@@ -0,0 +1,24 @@
+﻿using BacPatient.Application.Activity.Queries;
+using BacPatient.Application.testEvents;
+
+namespace BacPatient.Api.Endpoints
+{
+    public class testEvents : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/testEvents/{name}", async (string eventName, ISender sender) =>
+            {
+                var validName = eventName == "v" ? "validPrescriptionEvent" : "DiscontinuedPrescriptionEvent";
+                var result = await sender.Send(new TestEventQuery(validName));
+                return Results.Ok("sent : " + validName);
+            })
+            .WithName($"Get {nameof(testEvents)}")
+            .Produces<string>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary($"Get test")
+            .WithDescription($"Get test");
+        }
+    }
+}
