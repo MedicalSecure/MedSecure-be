d1d961219054802ba083d8fa20eda2c402bb5f83
aymen-elhajji <102991501+aymen-elhajji@users.noreply.github.com>
2024-07-17 21:09:27 +0100

    feat/add-out-patient-prescription-event

---
+++ src/Services/Registration/Registration.Application/Histories/EventHandlers/Integration/OutPatientPrescriptionEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Messaging.Events.PrescriptionEvents;
+using Registration.Application.Histories.Commands.CreateHistory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registration.Application.Histories.EventHandlers.Integration
+{
+    public class OutPatientPrescriptionEventHandler(ISender sender) : IConsumer<OutpatientPrescriptionSharedEvent>
+    {
+        public async Task Consume(ConsumeContext<OutpatientPrescriptionSharedEvent> context)
+        {
+            var pres = context.Message;
+            var newResidentHistory = new HistoryDto(HistoryStatus.Out, pres.RegisterId, null, DateTime.Now);//Id null to be created automatically
+            var command = new CreateHistoryCommand(newResidentHistory);
+            var response = await sender.Send(command);
+            Console.WriteLine($"New history created : {response.Id}");
+        }
+    }
+}
