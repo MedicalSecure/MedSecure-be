3737882d3428a595f00fb52487767e60409a13c5
aymen-elhajji <102991501+aymen-elhajji@users.noreply.github.com>
2024-07-19 20:58:03 +0100

    fix event OutPatient verification

--- src/Services/Registration/Registration.Application/Histories/EventHandlers/Integration/OutPatientPrescriptionEventHandler.cs
+++ src/Services/Registration/Registration.Application/Histories/EventHandlers/Integration/OutPatientPrescriptionEventHandler.cs
@@ -1,22 +1,52 @@
 ﻿using BuildingBlocks.Messaging.Events.PrescriptionEvents;
 using Registration.Application.Histories.Commands.CreateHistory;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Registration.Application.Histories.EventHandlers.Integration
 {
-    public class OutPatientPrescriptionEventHandler(ISender sender) : IConsumer<OutpatientPrescriptionSharedEvent>
+    public class OutPatientPrescriptionEventHandler : IConsumer<OutpatientPrescriptionSharedEvent>
     {
+        private readonly ISender _sender;
+        private readonly IApplicationDbContext _dbContext;
+
+        public OutPatientPrescriptionEventHandler(ISender sender, IApplicationDbContext dbContext)
+        {
+            _sender = sender;
+            _dbContext = dbContext;
+        }
+
         public async Task Consume(ConsumeContext<OutpatientPrescriptionSharedEvent> context)
         {
             var pres = context.Message;
-            var newResidentHistory = new HistoryDto(HistoryStatus.Out, pres.RegisterId, null, DateTime.Now);//Id null to be created automatically
-            var command = new CreateHistoryCommand(newResidentHistory);
-            var response = await sender.Send(command);
-            Console.WriteLine($"New history created : {response.Id}");
+
+            var register = await _dbContext.Registers
+             .Include(r => r.History)
+             .FirstOrDefaultAsync(r => r.Id == RegisterId.Of(new Guid(pres.RegisterId.ToByteArray())));
+
+            if (register == null)
+            {
+                Console.WriteLine("Register not found.");
+                return;
+            }
+
+            var latestHistory = register.History
+            .Where(h => h.RegisterId == register.Id)
+            .OrderByDescending(h => h.Date)
+            .FirstOrDefault();
+
+            if (latestHistory == null || latestHistory.Status != HistoryStatus.Registered)
+            {
+                Console.WriteLine("The status is not changeable.");
+                return;
+            }
+
+            else
+            {
+                var newResidentHistory = new HistoryDto(HistoryStatus.Out, pres.RegisterId, null, DateTime.Now);
+                var command = new CreateHistoryCommand(newResidentHistory);
+                var response = await _sender.Send(command);
+                Console.WriteLine($"New history created: {response.Id}");
+            }
+
         }
     }
 }
